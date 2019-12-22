8f6eade077f5d4d1dd7aedb7ac60916907e456c5
RDumancic <rdumancic@etfos.hr>
2019-12-22 15:50:52 +0100

    initial commit

---
+++ RUAP_6_2/RUAP_6_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
